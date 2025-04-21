be9f5abb12ecd46d70106ad4293057b49a366736
hovakimyan <liovaswagg@hotmail.com>
2025-04-21 13:31:16 +0200

    Tests unitaires fonctionnelles pour le MovingObstacle

--- core/src/main/java/fr/supdevinci/avoidme/MovingObstacle.java
+++ core/src/main/java/fr/supdevinci/avoidme/MovingObstacle.java
@@ -14,24 +14,33 @@ public class MovingObstacle {
     private float stateTime;
     private Rectangle bounds;
     private Vector2 velocity;
+    private boolean isTest;
 
     public MovingObstacle(float width, float height, float speed) {
-        String[] creatureFiles = {
-                "creature.png", "creature-2.png", "creature-3.png", "creature-4.png",
-                "creature-5.png", "creature-6.png", "creature-7.png", "creature-8.png",
-                "creature-9.png", "creature-10.png"
-        };
-        String selectedCreature = creatureFiles[MathUtils.random(creatureFiles.length - 1)];
-        Texture spriteSheet = new Texture(selectedCreature);
+        this(width, height, speed, false); // Par défaut, pas en mode test
+    }
 
-        int frameWidth = spriteSheet.getWidth() / 4;
-        int frameHeight = spriteSheet.getHeight();
-        TextureRegion[][] tmp = TextureRegion.split(spriteSheet, frameWidth, frameHeight);
-        TextureRegion[] frames = new TextureRegion[4];
-        System.arraycopy(tmp[0], 0, frames, 0, 4);
+    public MovingObstacle(float width, float height, float speed, boolean isTest) {
+        this.isTest = isTest;
 
-        animation = new Animation<>(0.1f, frames);
-        stateTime = 0f;
+        if (!isTest) {
+            String[] creatureFiles = {
+                    "creature.png", "creature-2.png", "creature-3.png", "creature-4.png",
+                    "creature-5.png", "creature-6.png", "creature-7.png", "creature-8.png",
+                    "creature-9.png", "creature-10.png"
+            };
+            String selectedCreature = creatureFiles[MathUtils.random(creatureFiles.length - 1)];
+            Texture spriteSheet = new Texture(selectedCreature);
+
+            int frameWidth = spriteSheet.getWidth() / 4;
+            int frameHeight = spriteSheet.getHeight();
+            TextureRegion[][] tmp = TextureRegion.split(spriteSheet, frameWidth, frameHeight);
+            TextureRegion[] frames = new TextureRegion[4];
+            System.arraycopy(tmp[0], 0, frames, 0, 4);
+
+            animation = new Animation<>(0.1f, frames);
+            stateTime = 0f;
+        }
 
         float x = 0, y = 0;
         int edge = MathUtils.random(3);
@@ -63,12 +72,24 @@ public class MovingObstacle {
     public void update(float delta) {
         bounds.x += velocity.x * delta;
         bounds.y += velocity.y * delta;
+
+        // Empêcher l'obstacle de sortir des limites du monde
+        if (bounds.x < -Constants.OBSTACLE_WIDTH)
+            bounds.x = -Constants.OBSTACLE_WIDTH;
+        if (bounds.x > Constants.WORLD_WIDTH + Constants.OBSTACLE_WIDTH)
+            bounds.x = Constants.WORLD_WIDTH + Constants.OBSTACLE_WIDTH;
+        if (bounds.y < -Constants.OBSTACLE_HEIGHT)
+            bounds.y = -Constants.OBSTACLE_HEIGHT;
+        if (bounds.y > Constants.WORLD_HEIGHT + Constants.OBSTACLE_HEIGHT)
+            bounds.y = Constants.WORLD_HEIGHT + Constants.OBSTACLE_HEIGHT;
     }
 
     public void render(SpriteBatch batch) {
-        stateTime += Gdx.graphics.getDeltaTime();
-        TextureRegion currentFrame = animation.getKeyFrame(stateTime, true);
-        batch.draw(currentFrame, bounds.x, bounds.y, bounds.width, bounds.height);
+        if (!isTest && animation != null) {
+            stateTime += Gdx.graphics.getDeltaTime();
+            TextureRegion currentFrame = animation.getKeyFrame(stateTime, true);
+            batch.draw(currentFrame, bounds.x, bounds.y, bounds.width, bounds.height);
+        }
     }
 
     public Rectangle getBounds() {
@@ -76,6 +97,8 @@ public class MovingObstacle {
     }
 
     public void dispose() {
-        animation.getKeyFrames()[0].getTexture().dispose();
+        if (!isTest && animation != null) {
+            animation.getKeyFrames()[0].getTexture().dispose();
+        }
     }
 }
